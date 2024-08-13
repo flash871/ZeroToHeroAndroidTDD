8d3080d5957b4347081fb5e014fca74d5bd2c48d
vovaserpo456@gmail.com <vovaserpov100@gmail.com>
2024-08-13 16:35:59 +0300

    task002 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var usualText = "I am an Android Developer!"
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         val textView = findViewById<TextView>(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.changeButton)
+
+        button.setOnClickListener {
+
+            textView.text = usualText
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("saveWord", usualText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val savedWord = savedInstanceState.getString("saveWord")
+        textView.text = savedWord
     }
 }
