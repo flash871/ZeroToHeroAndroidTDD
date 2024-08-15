43ac72c016a08d0563e2527eca2ac130229f7209
vovaserpo456@gmail.com <vovaserpov100@gmail.com>
2024-08-15 19:06:59 +0300

    task 002 ready for solving

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -5,23 +5,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.transition.Visibility
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    var VISIBILITY_STATE = false
+    lateinit var hideTextView: TextView
+    lateinit var hideButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val hideTextView = findViewById<TextView>(R.id.titleTextView)
-        val hideButton = findViewById<Button>(R.id.hideButton)
+         hideTextView = findViewById(R.id.titleTextView)
+         hideButton = findViewById(R.id.hideButton)
 
         hideButton.setOnClickListener {
-            hideTextView.visibility = TextView.INVISIBLE
+            VISIBILITY_STATE = true
+            hideTextView.visibility = if (VISIBILITY_STATE )TextView.INVISIBLE else TextView.VISIBLE
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putAll()
+        outState.putBoolean("visibilityState", VISIBILITY_STATE)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoreState = savedInstanceState.getBoolean("visibilityState")
+        hideTextView.visibility = if (restoreState )TextView.INVISIBLE else TextView.VISIBLE
     }
 
 }
